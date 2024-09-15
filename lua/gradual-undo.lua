64fca52fd48a38a2408917a7501c5e84fbf159cc
maksz42 <48691053+maksz42@users.noreply.github.com>
2024-09-15 04:28:46 +0200

    Fix undo/redo at end of line
    When undo/redo at the end of line, the cursor may shift horizontally by 1

--- lua/gradual-undo.lua
+++ lua/gradual-undo.lua
@@ -1,35 +1,42 @@
 local M = {}
 
 local function get_cursor_position()
-  return vim.fn.getpos('.')
+  return unpack(vim.api.nvim_win_get_cursor(0));
+end
+
+local function is_cursor_at_eol()
+  return vim.fn.col('.') >= vim.fn.col('$') - 1
+end
+
+local function undo_and_check_eol()
+  local cmd = 'undo'
+  vim.cmd(cmd)
+  return cmd, is_cursor_at_eol()
+end
+
+local function check_eol_and_redo()
+  local cmd = 'redo'
+  local eol = is_cursor_at_eol()
+  vim.cmd(cmd)
+  return cmd, eol
+end
+
+local function jump_to_last_do_position_or_do(todo, revert_cmd)
+  local row_before, col_before = get_cursor_position()
+  local cmd, eol = todo()
+  local row_after, col_after = get_cursor_position()
+  if row_before ~= row_after or (col_before ~= col_after and not eol) then
+    vim.cmd(revert_cmd)
+    print('gradual-undo: Jumped to last ' .. cmd .. ' location')
+  end
 end
 
 function M.jump_to_last_undo_position_or_undo()
-  local cursor_position_before = get_cursor_position()
-
-  vim.api.nvim_command('undo')
-
-  local cursor_position_after = get_cursor_position()
-
-  if not vim.deep_equal(cursor_position_before, cursor_position_after) then
-    vim.api.nvim_command('redo')
-
-    print('gradual-undo: Jumped to last undo location')
-  end
+  jump_to_last_do_position_or_do(undo_and_check_eol, 'redo')
 end
 
 function M.jump_to_last_redo_position_or_redo()
-  local cursor_position_before = get_cursor_position()
-
-  vim.api.nvim_command('redo')
-
-  local cursor_position_after = get_cursor_position()
-
-  if not vim.deep_equal(cursor_position_before, cursor_position_after) then
-    vim.api.nvim_command('undo')
-
-    print('gradual-undo: Jumped to last redo location')
-  end
+  jump_to_last_do_position_or_do(check_eol_and_redo, 'undo')
 end
 
 function M.setup(opts)
