134af9bc3cbec75806d0b99b367fc1a93fb44e9e
maksz42 <48691053+maksz42@users.noreply.github.com>
2024-09-16 03:28:14 +0200

    Fix delete at end of line
    The cursor doesn't move to the end of deleted text (and therefore to the end of line) when undo

--- lua/gradual-undo.lua
+++ lua/gradual-undo.lua
@@ -8,23 +8,12 @@ local function is_cursor_at_eol()
   return vim.fn.col('.') >= vim.fn.col('$') - 1
 end
 
-local function undo_and_check_eol()
-  local cmd = 'undo'
-  vim.cmd(cmd)
-  return cmd, is_cursor_at_eol()
-end
-
-local function check_eol_and_redo()
-  local cmd = 'redo'
+local function do_or_do_not(cmd, revert_cmd)
+  local row_before, col_before = get_cursor_position()
   local eol = is_cursor_at_eol()
   vim.cmd(cmd)
-  return cmd, eol
-end
-
-local function do_or_do_not(todo, revert_cmd)
-  local row_before, col_before = get_cursor_position()
-  local cmd, eol = todo()
   local row_after, col_after = get_cursor_position()
+  eol = eol or is_cursor_at_eol()
   if row_before ~= row_after or (col_before ~= col_after and not eol) then
     vim.cmd(revert_cmd)
     print('gradual-undo: Jumped to last ' .. cmd .. ' location')
@@ -32,11 +21,11 @@ local function do_or_do_not(todo, revert_cmd)
 end
 
 function M.undo()
-  do_or_do_not(undo_and_check_eol, 'redo')
+  do_or_do_not('undo', 'redo')
 end
 
 function M.redo()
-  do_or_do_not(check_eol_and_redo, 'undo')
+  do_or_do_not('redo', 'undo')
 end
 
 function M.setup(opts)
