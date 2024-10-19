f14387134e42b76c28932bef998a000f334c2e9e
maksz42 <48691053+maksz42@users.noreply.github.com>
2024-10-19 18:00:09 +0200

    Silence

--- lua/gradual-undo.lua
+++ lua/gradual-undo.lua
@@ -6,12 +6,13 @@ end
 
 local function do_or_do_not(cmd, revert_cmd)
   local pos_original = get_cursor_position()
-  vim.cmd(cmd)
+  local cmd_output = vim.api.nvim_exec2(cmd, { output = true }).output
   local pos_after_cmd = get_cursor_position()
   if vim.deep_equal(pos_original, pos_after_cmd) then
+    print(cmd_output)
     return
   end
-  vim.cmd(revert_cmd)
+  vim.cmd('silent ' .. revert_cmd)
   local pos_after_revert = get_cursor_position()
   if vim.deep_equal(pos_original, pos_after_revert) then
     vim.cmd(cmd)
